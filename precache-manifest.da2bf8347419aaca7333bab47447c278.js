1a33b0dc4595f3b9831aa978dbe594d0a4ffbd4c
chekwech <chekmasaiah@gmail.com>
2020-05-28 15:55:49 +0300

    Updates

---
+++ precache-manifest.da2bf8347419aaca7333bab47447c278.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d88b50f703209bb5d077b6916d4081b7",
+    "url": "/todoReactApp/index.html"
+  },
+  {
+    "revision": "130790849ac8389c40bc",
+    "url": "/todoReactApp/static/css/2.829c9cb5.chunk.css"
+  },
+  {
+    "revision": "c942cf7602c7b3bdc21c",
+    "url": "/todoReactApp/static/css/main.5ecd60fb.chunk.css"
+  },
+  {
+    "revision": "130790849ac8389c40bc",
+    "url": "/todoReactApp/static/js/2.8ba88a27.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/todoReactApp/static/js/2.8ba88a27.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "c942cf7602c7b3bdc21c",
+    "url": "/todoReactApp/static/js/main.39a52fe1.chunk.js"
+  },
+  {
+    "revision": "2a8840e53edc05feb8d6",
+    "url": "/todoReactApp/static/js/runtime-main.0aa5b721.js"
+  }
+]);
